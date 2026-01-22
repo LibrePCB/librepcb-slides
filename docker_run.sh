ef1645cf05fcd985e6a5a620810936e098c3e800
U. Bruhin <urbibruhin@bluewin.ch>
2026-01-22 14:35:35 +0100

    Remove LaTeX slides

--- docker_run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-DIR=$(dirname "$(readlink -f "$0")")
-docker run --rm --user `id -u`:`id -g` -v "$DIR:/source" librepcb-slides
--- make.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-latexmk -pdf -shell-escape librepcb-slides.tex
